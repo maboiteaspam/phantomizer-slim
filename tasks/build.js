790f644964c978e97cea5adc0b8887626b411d12
clement <maboiteaspam@wanadoo.fr>
2013-10-21 21:23:38 +0800

    Initial commit

---
+++ tasks/build.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+
+    var fs = require("fs");
+    var path = require("path");
+    var slim = require("../lib/slim.js");
+
+    function copy_recusive( source, pattern, output ){
+        var files = grunt.file.expand({}, [source+pattern]);
+        for( var n in files ){
+            var f = files[n].substring(source.length)
+            if( f != "" ){
+                if( grunt.file.isDir(files[n]) ){
+                    grunt.file.mkdir( output+"/"+f )
+                }else{
+                    var output_f = (output+"/"+f).replace("//","/")
+                    grunt.file.copy(source+"/"+f, output_f)
+                    grunt.verbose.ok("copying "+source+"/"+f+" "+output_f);
+                }
+            }
+        }
+    }
+    
+    grunt.registerMultiTask("phantomizer-export-slim", "", function () {
+        var done = this.async();
+
+        var options = this.options();
+
+        var export_dir = options.export_dir;
+
+        grunt.log.ok("exporting to "+export_dir );
+        grunt.file.mkdir( export_dir );
+
+        var source = slim.www_vendors_path;
+        var files = grunt.file.expand({}, [source+"**"]);
+        for( var n in files ){
+            var f = files[n].substring(source.length)
+            if( f != "" ){
+                if( grunt.file.isDir(files[n]) ){
+                    grunt.file.mkdir( export_dir+"/"+f )
+                }else{
+                    var output_f = path.normalize(export_dir+"/"+f)
+                    grunt.file.copy(source+"/"+f, output_f)
+                    grunt.verbose.ok("copying "+source+"/"+f+" "+output_f);
+                }
+            }
+        }
+        done();
+    });
+
+};
