16eeb6311c591003bfa6a40eff7a5fcc674029bb
clement <maboiteaspam@wanadoo.fr>
2014-02-15 02:25:32 +0800

    code style

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 
 module.exports = function(grunt) {
 
-    grunt.initConfig();
+  grunt.initConfig();
 
-    grunt.loadNpmTasks('phantomizer-dir-inject-html-extras');
+  grunt.loadNpmTasks('phantomizer-dir-inject-html-extras');
 };
--- tasks/build.js
+++ tasks/build.js
@@ -3,35 +3,35 @@
 module.exports = function(grunt) {
 
 
-    var fs = require("fs");
-    var path = require("path");
-    var slim = require("../lib/slim.js");
+  var fs = require("fs");
+  var path = require("path");
+  var slim = require("../lib/slim.js");
 
-    grunt.registerMultiTask("phantomizer-export-slim", "", function () {
-        var done = this.async();
+  grunt.registerMultiTask("phantomizer-export-slim", "", function () {
+    var done = this.async();
 
-        var options = this.options();
+    var options = this.options();
 
-        var export_dir = options.export_dir;
+    var export_dir = options.export_dir;
 
-        grunt.log.ok("exporting to "+export_dir );
-        grunt.file.mkdir( export_dir );
+    grunt.log.ok("exporting to "+export_dir );
+    grunt.file.mkdir( export_dir );
 
-        var source = slim.documentroot_path;
-        var files = grunt.file.expand({}, [source+"/**"]);
-        for( var n in files ){
-            var f = files[n].substring(source.length)
-            if( f != "" ){
-                if( grunt.file.isDir(files[n]) ){
-                    grunt.file.mkdir( export_dir+"/"+f )
-                }else{
-                    var output_f = path.normalize(export_dir+"/"+f)
-                    grunt.file.copy(source+"/"+f, output_f)
-                    grunt.verbose.ok("copying "+source+"/"+f+" "+output_f);
-                }
-            }
+    var source = slim.documentroot_path;
+    var files = grunt.file.expand({}, [source+"/**"]);
+    for( var n in files ){
+      var f = files[n].substring(source.length)
+      if( f != "" ){
+        if( grunt.file.isDir(files[n]) ){
+          grunt.file.mkdir( export_dir+"/"+f )
+        }else{
+          var output_f = path.normalize(export_dir+"/"+f)
+          grunt.file.copy(source+"/"+f, output_f)
+          grunt.verbose.ok("copying "+source+"/"+f+" "+output_f);
         }
-        done();
-    });
+      }
+    }
+    done();
+  });
 
 };
